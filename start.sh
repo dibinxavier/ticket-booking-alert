ec61203dc97d3e2046426909189126d67599388e
DiBiN <41875571+dibinxavier@users.noreply.github.com>
2024-05-18 14:01:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+
+
+
+#!/bin/bash
+
+# Add any setup commands here
+echo "Running setup commands..."
+
+# Example: Setting executable permissions for ChromeDriver
 chmod +x ./chromedriver
+
+# Start your Python script
+echo "Starting Python script..."
+python3 alert.py
