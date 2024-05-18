e922f3f9b15322c96098c663c6bf157827e81aff
DiBiN <41875571+dibinxavier@users.noreply.github.com>
2024-05-18 15:14:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,24 @@
 # heroku buildpacks:add heroku/google-chrome
 # heroku buildpacks:add heroku/chromedriver
 
+#!/bin/bash
+
+# Install Google Chrome
+echo "Install Google Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+echo "Install ChromeDriver..."
+CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+chmod +x chromedriver
+mv -f chromedriver /usr/local/bin/chromedriver
+
 # Add any setup commands here
 echo "Running setup commands..."
 
