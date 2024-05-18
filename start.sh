62b437be9ccafe4cb89c00a1a781e12434cbad40
DiBiN <41875571+dibinxavier@users.noreply.github.com>
2024-05-18 15:26:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,30 +3,29 @@
 # heroku buildpacks:add heroku/google-chrome
 # heroku buildpacks:add heroku/chromedriver
 
-#!/bin/bash
-
 # Install Google Chrome
 echo "Install Google Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+sudo rm google-chrome-stable_current_amd64.deb
 
 # Install ChromeDriver
 echo "Install ChromeDriver..."
 CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
-chmod +x chromedriver
-mv -f chromedriver /usr/local/bin/chromedriver
+sudo wget -N https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
+sudo rm chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo mv -f chromedriver /usr/local/bin/chromedriver
 
 # Add any setup commands here
 echo "Running setup commands..."
 
 # Example: Setting executable permissions for ChromeDriver
-chmod +x ./chromedriver
+sudo chmod +x /usr/local/bin/chromedriver
 
 # Start your Python script
 echo "Starting Python script..."
 python3 alert.py
+
